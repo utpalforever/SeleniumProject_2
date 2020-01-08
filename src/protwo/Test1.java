8416dd451e240459ac1908872bd19124bea979d8
Shivam <utpalforever@gmail.com>
2020-01-08 22:51:31 +0530

    s

--- src/protwo/Test1.java
+++ src/protwo/Test1.java
@@ -5,6 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Checking git commit");
 		System.out.println("Checking stash commit");
+		System.out.println("Checking branch   co mmit");
+		
 
 	}
 

1cc8c10d2ebd80222ee3a2bc623505344183673e
unknown <utpalforever@gmail.com>
2020-01-08 22:12:13 +0530

    checking diff

--- src/protwo/Test1.java
+++ src/protwo/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Checking git commit");
 		System.out.println("Checking stash commit");
+		System.out.println("Checking branch commit");
 
 	}
 

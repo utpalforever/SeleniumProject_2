67a2e0eaf42752d55652ef15441df0c771f713a0
Shivam <utpal.shivam@accenture.com>
2020-02-22 18:40:50 +0530

    test

---
+++ src/proone/Feature1.java
@@ -0,0 +1,16 @@
+package proone;
+
+public class Feature1 {
+
+	public static void f2() {
+		System.out.println("f2");
+	}
+	public static void main(String[] args) {
+
+
+		System.out.println("f2");
+
+		f2();
+	}
+
+}

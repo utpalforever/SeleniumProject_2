dc0effd6b36a01d7f5fcb1a8f60f9b4826c67cfb
Shivam <utpal.shivam@accenture.com>
2020-02-22 18:31:57 +0530

    Feature2

---
+++ src/proone/Fearure2.java
@@ -0,0 +1,15 @@
+package proone;
+
+public class Fearure2 {
+
+	public static void f1() {
+		System.out.println("feature2");
+	}
+	public static void main(String[] args) {
+
+System.out.println("Feature2");
+f1();
+
+	}
+
+}

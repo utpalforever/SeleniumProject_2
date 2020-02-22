e9ce5779f05fca2777b948edd759bc297f094265
utpal.shivam <utpal.shivam@P4ALAP-HC2H0G2.dir.svc.accenture.com>
2020-02-22 17:30:36 +0530

    Revert "s"
    This reverts commit 8416dd451e240459ac1908872bd19124bea979d8.

--- src/protwo/Test1.java
+++ src/protwo/Test1.java
@@ -5,8 +5,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Checking git commit");
 		System.out.println("Checking stash commit");
-		System.out.println("Checking branch   co mmit");
-		
 
 	}
 
